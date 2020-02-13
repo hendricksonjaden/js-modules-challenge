989a22ca381a57008725a553bc2855393a7bc8ab
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-02-12 18:24:32 -0700

    first commit

---
+++ src/bootstrap.js
@@ -0,0 +1,7 @@
+import classChallenge, {variable, myFunction} from "./classChallenge"
+
+console.log(variable)
+console.log(myFunction(2, 3))
+console.log(classChallenge())
+
+
---
+++ src/classChallenge.js
@@ -0,0 +1,9 @@
+export default function() {
+    return `<div>classChallenge</div>`
+}
+
+export const variable = 'Welcome' 
+
+export function myFunction(p1, p2) {
+    return (p1 + p2); 
+}
